c8246a69ea296a98b7eed4a23ad69f7ff5348ce5
DidaPradana <42725223+didapradana@users.noreply.github.com>
2018-11-05 10:04:22 +0700

    Add files via upload

---
+++ jurnal10-DidaPradana-6701171118/lihatData.php
@@ -0,0 +1,42 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+</head>
+<body>
+	<br>
+	<a href="dashboard.php"><button type="button" class="btn btn-success">HOME</button></a>
+	<br>
+	<br>
+	 <table border="1" width="40%" class="table table-hover">
+	 	
+	 	<tr class="table-primary">
+	 		<th>Username</th>
+	 		<th>Password</th>
+	 	</tr>
+
+	 	<?php
+			require_once ("class.php");
+			$result = $proses->viewprofile();
+	 		if (mysqli_num_rows($result)>0) {
+	 			while ($row = mysqli_fetch_assoc($result)) {
+	 				?>
+	 				<tr>
+	 				<td><?php echo $row['username']?></td>
+	 				<td><?php echo $row['password']?></td>
+	 				</tr>
+	 				<?php
+	 			}
+	 		}else{
+	 			echo "0 results";
+	 		}
+	 		
+	 	?>
+	 </table>
+
+</body>
+</html>
